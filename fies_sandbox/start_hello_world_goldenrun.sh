b8e68b5f5a7d5ef12f8f88ec4f212bc830f3697b
Andrea Hoeller <hoeller.andrea@gmail.com>
2016-06-16 16:54:30 +0200

    add examples

---
+++ fies_sandbox/start_hello_world_goldenrun.sh
@@ -0,0 +1,1 @@
+../arm-softmmu/qemu-system-arm -semihosting -kernel example_binaries/hello_world
---
+++ fies_sandbox/start_hello_world_with_fi.sh
@@ -0,0 +1,1 @@
+../arm-softmmu/qemu-system-arm -semihosting -kernel example_binaries/hello_world -fi fault_memory_cell_saf.xml
